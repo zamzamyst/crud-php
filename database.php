7d9b3aa48f52e49fdedaff02657bde7ef9fdd33b
zamzamyst <sultan.afzam@gmail.com>
2025-03-09 18:11:51 +0700

    updated: database.php

--- database.php
+++ database.php
@@ -1,5 +1,5 @@
 <?php
-    $db_connection = new mysqli("localhost", "zamzamyst", "Dreamzzz@19", "wad_handson");
+    $db_connection = new mysqli("localhost", "root", "", "wad_handson");
     if (!$db_connection) 
         die("db_connection gagal: " . mysqli_connect_error());
 ?>
